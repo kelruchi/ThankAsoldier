daf1f4d93d037d2233de97c5a57e2c1a0ddb70f2
ugookoro <okorougoc@gmail.com>
2015-12-03 12:10:42 +0100

    Boilerplate

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thank A Soldier</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/thankasoldier.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-custom" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header img-responsive">
+                    <button type="button" class="navbar-toggle" style="margin-right:0; color:#fff; padding:0;" data-toggle="collapse" data-target=".navbar-main-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <h3>
+                        <a class=" page-scroll" href="#page-top">
+                            <img src="images/TASLOGO1.png" alt="Thank_A_Soldier_Logo" style="width:11rem;" />
+                        </a>
+                    </h3>
+                    <!-- <a class=" page-scroll" href="#page-top">
+                            <img src="images/lo.png" class="img img-responsive" alt="" style="width:10rem" />
+                        </a> -->
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                    <ul class="nav navbar-nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li >
+                            <a href="#page-top">Thank A Soldier</a>
+                        </li>
+                        <li>
+                            <a href="#">Gratitude Wall</a>
+                        </li>
+                        <li>
+                            <a href="#">About The Campaign</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+    </header>
+    <section class="content-section">
+        <div class="container-fluid">
+            <div class="row-fluid">
+            </div>
+        </div>
+    </section>
+    <footer class="content-section">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="col-md-3 col-xs-12">
+                    <h5 style="margin-top:10px">#ThankASoldier is hosted by</h5>
+                    <img src="images/anaklelogo.png" alt="anakle_logo_thankASoldier" class="img img-responsive anakle" />
+                </div>
+
+                <div class="col-md-9 col-xs-12">
+                    <h5 style="margin-top:10px">Proudly supported by</h5>
+                    <div class="col-sm-3 col-xs-6">
+                        <img src="images/andelalogo.png" alt="andela_logo_thankASoldier" class="img img-responsive partner" />
+                    </div>
+                    <div class="col-sm-3 col-xs-6" >
+                            <img src="images/swifta.png" alt="anakle_logo_thankASoldier"  class="img img-responsive partner"/>
+                    </div>
+                    <div class="col-sm-3 col-xs-6">
+                        <img src="images/printivo_logo.png" style="background-color:#fff;" alt="anakle_logo_thankASoldier"  class="img img-responsive partner"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/grayscale.js"></script>
+</body>
+</html>
